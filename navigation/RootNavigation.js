af92d317a56c94b31e96ad5bacbaf74f3209b3b4
IgodoNdu <darkbluetechmail@gmail.com>
2022-11-13 05:21:04 +0100

    Update RootNavigaton
    Adding signup screen to the stack navigation

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Welcome from '../screens/Welcome';
-import { Login } from '../screens/auth';
+import { Login, Signup } from '../screens/auth';
 
 export default function RootNavigation () {
   //creating the app's view stack
@@ -17,6 +17,7 @@ export default function RootNavigation () {
         <Stack.Navigator screenOptions={screenOptions}>
           <Stack.Screen name="Splash" component={Welcome} />
           <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
         </Stack.Navigator>
       </NavigationContainer>
     )
