9e9ad1a3ed4762bbc769e1843dbe0f6497bbea54
IgodoNdu <darkbluetechmail@gmail.com>
2022-11-13 02:10:12 +0100

    Button and its styling
    Button jsx and it's styling

--- components/Buttons/Button.jsx
+++ components/Buttons/Button.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import { Image, Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native'
+import style from './style';
 
 export default function Button(props) {
-    const {text, icon, disable } = props;
+    const {text, icon, disable, btnStyle, onPress, btnTextStyle } = props;
 
     return (
-      <TouchableOpacity activeOpacity={0.5}>
-        {text && <Text>{text}</Text>}
-        {icon && <Image source={icon} resizeMode="contain" />}
+      <TouchableOpacity 
+        activeOpacity={0.5}
+        onPress={onPress}
+        disable={disable}
+        style={[style.btnContainer, btnStyle]}
+
+      >
+        {text && 
+          <Text style={[style.btnText, btnTextStyle, {marginRight: icon ? 10 : 0}]}>
+            {text}
+          </Text>}
+        {icon && <Image source={icon} resizeMode="contain"  style={style.iconStyle} />}
       </TouchableOpacity>
     )
 }
